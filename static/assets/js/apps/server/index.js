709db27f69e1a203e2286c4c46f35196d58099c1
巫书轶 <wushuyi541@gmail.com>
2015-02-06 14:08:24 +0800

    2015-02-06 v1

--- static/assets/js/apps/server/index.js
+++ static/assets/js/apps/server/index.js
@@ -13,33 +13,5 @@ define([
     _
 ){
     'use strict';
-    var swalTest, magnificTest;
-    window.swalTest = swalTest = function(){
-        swal({
-            title: '网络出现错误...',
-            text: '',
-            type: 'error',
-            allowOutsideClick: false,
-            showCancelButton: false,
-            closeOnConfirm: true,
-            closeOnCancel: true,
-            confirmButtonText: 'OK',
-            confirmButtonColor: '#AEDEF4',
-            cancelButtonText: 'Cancel',
-            imageUrl: null,
-            imageSize: null,
-            timer: null,
-            customClass: '',
-            html: false,
-            animation: true,
-            allowEscapeKey: true
-        });
-    };
-
-    window.magnificTest = magnificTest = function(){
-        $('.image-link').magnificPopup({type:'image'});
-    };
-
-
-
+    
 });
